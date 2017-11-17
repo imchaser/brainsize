cb4a24d4e102d682e4d78430236161012a7a74d2
VanTranHuy <huyherohh98@gmail.com>
2017-11-17 12:14:16 +0700

    giao diện Strart game , RankScreen , HelpScreen

--- BrainSize/src/presentations/HelpScreen.java
+++ BrainSize/src/presentations/HelpScreen.java
@@ -10,21 +10,67 @@ public class HelpScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        btnBackHelp = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(600, 600));
+        setMinimumSize(new java.awt.Dimension(600, 600));
+        setSize(new java.awt.Dimension(600, 600));
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jTextArea1.setText("+ Sau khi giao diện trò chơi được mở bạn sẽ thấy\n xuất hiêjn giao diện của trò chơi\n+ Bạn có thể click để chơi ngay hoặc xem bảng xếp\n hạng nế đã từng chơi hoặc ấn thoát ngay\n+ Trò chơi bao gồm 15 câu hỏi chia làm ba mốc 5,\n 10 và 15 kết thúc 15 câu hỏi bạn sẽ đạt tối đa 15\n điểm và được lưu tên lại \n+ Trong quá trình chơi game bạn có 2 sự chơi giúp \n đó là 50/50 bạn được phép loại bỏ đi 2 phương án \n sai , và có quyền next câu hỏi tuy nhiên việc next\n câu hỏi đồng nghĩa bạn sẽ bị trừ đi 1 điểm .\n+ kết thúc trò chơi ban sẽ được lưu điểm và có thể\n ấn tiếp tục để chơi tiếp \n+ chúc các bạn chơi game vui vẻ !.");
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(153, 0, 0));
+        jLabel1.setText("CÁCH CHƠI :");
+
+        btnBackHelp.setBackground(new java.awt.Color(0, 102, 102));
+        btnBackHelp.setIcon(new javax.swing.ImageIcon("C:\\Users\\huy\\Documents\\GitHub\\brainsize\\BrainSize\\src\\images\\images.png")); // NOI18N
+        btnBackHelp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackHelpActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(102, 102, 102)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnBackHelp, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 409, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(89, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(150, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(43, 43, 43)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(btnBackHelp, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(103, 103, 103))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnBackHelpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackHelpActionPerformed
+        // TODO add your handling code here:
+        StartGame st = new StartGame();
+        st.setVisible(true);
+        this.setVisible(false);
+       
+    }//GEN-LAST:event_btnBackHelpActionPerformed
 
     /**
      * @param args the command line arguments
@@ -62,5 +108,9 @@ public class HelpScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBackHelp;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
--- BrainSize/src/presentations/RankScreen.java
+++ BrainSize/src/presentations/RankScreen.java
@@ -10,21 +10,94 @@ public class RankScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pnRank = new javax.swing.JPanel();
+        lblRank = new javax.swing.JLabel();
+        txtTop1 = new javax.swing.JTextField();
+        txtTop2 = new javax.swing.JTextField();
+        txtTop3 = new javax.swing.JTextField();
+        txtTop4 = new javax.swing.JTextField();
+        txtTop5 = new javax.swing.JTextField();
+        btnBackRank = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(600, 600));
+        setMinimumSize(new java.awt.Dimension(600, 600));
+        setSize(new java.awt.Dimension(600, 600));
+
+        lblRank.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        lblRank.setForeground(new java.awt.Color(153, 0, 0));
+        lblRank.setText("BẢNG ĐIỂM");
+
+        btnBackRank.setIcon(new javax.swing.ImageIcon("C:\\Users\\huy\\Documents\\GitHub\\brainsize\\BrainSize\\src\\images\\images.png")); // NOI18N
+        btnBackRank.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackRankActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout pnRankLayout = new javax.swing.GroupLayout(pnRank);
+        pnRank.setLayout(pnRankLayout);
+        pnRankLayout.setHorizontalGroup(
+            pnRankLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnRankLayout.createSequentialGroup()
+                .addGap(169, 169, 169)
+                .addGroup(pnRankLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(pnRankLayout.createSequentialGroup()
+                        .addComponent(txtTop2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(135, 221, Short.MAX_VALUE))
+                    .addGroup(pnRankLayout.createSequentialGroup()
+                        .addGroup(pnRankLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtTop1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtTop3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtTop5, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtTop4, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnBackRank, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))))
+            .addGroup(pnRankLayout.createSequentialGroup()
+                .addGap(204, 204, 204)
+                .addComponent(lblRank)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        pnRankLayout.setVerticalGroup(
+            pnRankLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnRankLayout.createSequentialGroup()
+                .addContainerGap(162, Short.MAX_VALUE)
+                .addComponent(lblRank)
+                .addGap(46, 46, 46)
+                .addComponent(txtTop1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
+                .addComponent(txtTop2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
+                .addComponent(txtTop3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(38, 38, 38)
+                .addComponent(txtTop5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(txtTop4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(btnBackRank, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(58, 58, 58))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(pnRank, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(pnRank, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnBackRankActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackRankActionPerformed
+        // TODO add your handling code here:
+        StartGame st = new StartGame();
+        st.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnBackRankActionPerformed
 
     /**
      * @param args the command line arguments
@@ -63,5 +136,13 @@ public class RankScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBackRank;
+    private javax.swing.JLabel lblRank;
+    private javax.swing.JPanel pnRank;
+    private javax.swing.JTextField txtTop1;
+    private javax.swing.JTextField txtTop2;
+    private javax.swing.JTextField txtTop3;
+    private javax.swing.JTextField txtTop4;
+    private javax.swing.JTextField txtTop5;
     // End of variables declaration//GEN-END:variables
 }
